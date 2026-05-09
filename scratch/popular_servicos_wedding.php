2d245503db9ebaf31b337e55dc7728b4bd98a6f9
lindavoz <soulindavoz@gmail.com>
2026-05-09 11:30:28 -0300

    Update popular_servicos_wedding.php

--- scratch/popular_servicos_wedding.php
+++ scratch/popular_servicos_wedding.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/includes/helpers.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/helpers.php';
 
 $db = Database::get();
 
