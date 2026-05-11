144e16101540d88f5183d6f7ddcecd777e957b29
lindavoz <soulindavoz@gmail.com>
2026-05-11 15:45:26 -0300

    Create inspect_propostas.php

---
+++ inspect_propostas.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/config/database.php';
+
+try {
+    $db = Database::get();
+    $stmt = $db->query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'propostas'");
+    $columns = $stmt->fetchAll();
+    foreach ($columns as $col) {
+        echo $col['column_name'] . " (" . $col['data_type'] . ")\n";
+    }
+} catch (Exception $e) {
+    echo "Erro: " . $e->getMessage() . "\n";
+}
