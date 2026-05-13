5c87a81d4d72b21a1936af0e9f96dc635fa09e0c
comunicacaoiblvitoria-lgtm <comunicacao.iblvitoria@gmail.com>
2026-05-13 16:01:28 -0300

    Update env.php

--- sistema/config/env.php
+++ sistema/config/env.php
@@ -1,10 +1,10 @@
 <?php
 // Copie este arquivo para env.php e preencha os valores reais
-define('DB_HOST', 'srv952.hstgr.io');
-define('DB_NAME', 'u306254544_distinto');
-define('DB_USER', 'u306254544_poncem');
-define('DB_PASS', '!@Jeane&w#1');
-define('DB_PORT', 3306);
+define('DB_HOST', 'ep-crimson-sun-ac4t9f9a-pooler.sa-east-1.aws.neon.tech');
+define('DB_NAME', 'neondb');
+define('DB_USER', 'neondb_owner');
+define('DB_PASS', 'npg_3fXdCHMbS2xJ');
+define('DB_PORT', 5432);
 
 define('GROQ_API_KEY', 'SUA_GROQ_KEY_AQUI');
 define('GROQ_MODEL', 'llama-3.3-70b-versatile');
