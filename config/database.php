ef8e600399e2a58b3873000743c6b80514703fea
comunicacaoiblvitoria-lgtm <comunicacao.iblvitoria@gmail.com>
2026-06-29 01:48:28 -0300

    fix pgsql dsn - remove options param that breaks neon connections

--- config/database.php
+++ config/database.php
@@ -11,9 +11,7 @@ class Database {
                     // Conexão MySQL (Hostinger)
                     $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4';
                 } else {
-                    // Conexão PostgreSQL (Neon)
-                    $endpoint = explode('.', DB_HOST)[0];
-                    $dsn = 'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';sslmode=require;options=\'endpoint=' . $endpoint . '\'';
+                    $dsn = 'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';sslmode=require';
                 }
                 
                 self::$instance = new PDO($dsn, DB_USER, DB_PASS, [
